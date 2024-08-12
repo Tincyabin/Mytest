538daa58bdfb53920349794a158374d1825fc144
TINCY <tincystc@gmail.com>
2024-08-12 11:12:45 +0530

    initialfiles

---
+++ connection/src/connection/Demo.java
@@ -0,0 +1,15 @@
+package connection;
+
+import demo.ChromeDriver;
+import demo.WebDriver;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Hp\\Downloads\\selenium automation\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+	}
+
+}
